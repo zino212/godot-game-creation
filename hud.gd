783ee78bc7ca5b285c5ea883a5f8314c4865bec5
LuisGuenther <luis.guenther@maibornwolff.de>
2023-08-01 11:47:39 +0200

    Make HUD visible on game start

--- hud.gd
+++ hud.gd
@@ -4,7 +4,7 @@ signal escape
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
