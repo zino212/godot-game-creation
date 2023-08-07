db34013d536c8b51cacf378d9105b3daef1ce420
zino212 <luisguenther@gmx.de>
2023-08-07 14:28:00 +0200

    Add hurt and game over audio

--- MissileShot.gd
+++ MissileShot.gd
@@ -4,7 +4,4 @@ func _ready():
 	$Dust.one_shot = true
 	$Debris.one_shot = true
 	$Sparks.play("Explosion")
-<<<<<<< HEAD
 	$AudioStreamPlayer.play()
-=======
->>>>>>> bfb0ee6 (Add screen shake and explosion)
