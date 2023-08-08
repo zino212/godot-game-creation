d6a36bb90afdab7d93bf4d3e31d0bf0843eaab70
LuisGuenther <luis.guenther@maibornwolff.de>
2023-08-08 10:40:16 +0200

    Tweak explosion and refactor audio

--- audioPlayer.gd
+++ audioPlayer.gd
@@ -4,12 +4,17 @@ var shield_audio = preload("res://Audio/powerUp.wav")
 var missile_audio = preload("res://Audio/laserShoot.wav")
 var heart_audio = preload("res://Audio/lifeUp.wav")
 
+var hurt_audio = preload("res://Audio/hitHurt.wav")
 
-func play_audio(type):
+func play_item_audio(type):
 	if type == 0:
 		stream = shield_audio
 	elif type == 1:
 		stream = missile_audio
 	elif type == 2:
 		stream = heart_audio
+	play()
+
+func play_hurt_audio():
+	stream = hurt_audio
 	play()
